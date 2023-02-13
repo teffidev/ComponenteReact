569bd42f57cc01ba36066a3c09707a590319c5f7
teffidev <tefyberal09@gmail.com>
2023-02-13 14:40:52 -0500

    First Commit

---
+++ button.js
@@ -0,0 +1,18 @@
+//Este icono nos permite crear elementos dentro de React.
+const e = React.createElement;
+
+//¡Este es un componente React!
+const Button = () => e('button', {
+    onclick:() => alert("Has hecho click")},
+    'Haz Click!'
+);
+
+//Seleccionamos el "div" dentro de nuestro index.html
+const domContainer = document.querySelector('#button_container');
+
+//Aquí llamamos a un método de React que crea la base
+//para poder utilizar la librería dentro de nuestro <div>.
+const root = ReactDOM.createRoot(domContainer);
+
+//Con el método render vamos a mostrar el componente Button.
+root.render(e(Button));
